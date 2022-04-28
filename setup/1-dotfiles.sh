96f07a76ef505fea4b5c1d12407eb49a26ba529d
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2022-04-28 11:46:57 -0300

    shell setup

--- setup/1-dotfiles.sh
+++ setup/1-dotfiles.sh
@@ -3,6 +3,9 @@
 git clone git@github.com:thoughtbot/dotfiles.git ~/dotfiles
 git clone git@github.com:luizsignorelli/dotfiles.git ~/dotfiles-local
 
+brew install starship
+brew install zsh-autosuggestions
 brew install rcm
 
 env RCRC=$HOME/dotfiles/rcrc rcup
+
